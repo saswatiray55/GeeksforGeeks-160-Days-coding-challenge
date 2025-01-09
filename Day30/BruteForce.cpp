b3dd15fc6dccb86a548b22c290d6c393955a50f6
Saswati Ray <raysaswati195@gmail.com>
2025-01-09 20:13:57 +0530

    Day30

---
+++ Day30/BruteForce.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& arr, int key)
+{
+    int n = arr.size();
+    int result = -1;
+    for(int i = 0; i<n; i++)
+    {
+        if(key == arr[i])
+        {
+            result = i;
+        }
+    }
+    return result;
+
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size: ";
+    cin >> n;
+    vector<int> arr;
+    for(int i = 0; i<n; i++)
+    {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    int key;
+    cin >> key;
+
+    int ans = search(arr, n);
+    cout << ans << endl;
+}
