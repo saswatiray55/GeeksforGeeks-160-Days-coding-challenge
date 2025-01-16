4568dd8da0523d9d3d8969b6e0043bde28e8dce1
Saswati Ray <raysaswati195@gmail.com>
2025-01-16 21:10:08 +0530

    Day44

---
+++ Day44/BruteForce.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> findTriplets(vector<int>& arr)
+{
+    int n = arr.size();
+    vector<vector<int>> res;
+    for(int i = 0; i<n-2; i++)
+    {
+        for(int j =i+1; j<n-1; j++)
+        {
+            for(int k = j+1; k <n; k++)
+            {
+                if(arr[i] + arr[j] + arr[k] == 0)
+                {
+                    res.push_back({i, j, k});
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size: ";
+    cin >> n;
+
+    vector<int> arr;
+    for(int i = 0; i<n; i++)
+    {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    vector<vector<int>> res = findTriplets(arr);
+    for(int i = 0; i<res.size(); i++)
+    {
+        cout <<"[" << res[i][0] << " " << res[i][1] << " " << res[i][2] <<"]" << endl;
+    }
+    
+    return 0;
+}
+
+/*
+Time Complexity: O(n^3)
+*/
