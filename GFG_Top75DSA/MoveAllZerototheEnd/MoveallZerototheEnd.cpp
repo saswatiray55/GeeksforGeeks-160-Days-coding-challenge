bd503b070d366502bfecbbdaf2e84f720ffe4eb0
Saswati Ray <raysaswati195@gmail.com>
2025-02-13 23:14:53 +0530

    MoveZerototheEnd

---
+++ GFG_Top75DSA/MoveAllZerototheEnd/MoveallZerototheEnd.cpp
@@ -0,0 +1,56 @@
+/*283. Move All Zeroes to End
+
+You are given an array arr[] of non-negative integers. Your task is to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+
+Examples:
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+
+Input: arr[] = [10, 20, 30]
+Output: [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
+
+Constraints:
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 105
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pushZerosToEnd(vector<int>& arr) 
+{
+    int n = arr.size();
+    int count = 0;
+    for(int i = 0; i<n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            swap(arr[count], arr[i]);
+            count++;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size: ";
+    cin >> n;
+    vector<int>arr(n);
+    for(int i = 0; i<n; i++)
+    {
+        cin >>arr[i];
+    }
+    pushZerosToEnd(arr);
+    for(auto i : arr)
+    {
+        cout << i<< " ";
+    }
+    return 0;
+}
